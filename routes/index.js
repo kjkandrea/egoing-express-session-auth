94bdbe814ef8f1d097a56a901a263a19a89bc7dd
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-07-06 18:22:54 +0900

    인증 구현 : 인증 상태를 UI에 반영

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,25 @@ const express = require('express')
 const router = express.Router()
 const template = require('../lib/template.js')
 
+function authIsOwner(req, res){
+  if(req.session.is_logged_in){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function authStatusUI(req, res) {
+  let authStatusUI = '<a href="/auth/login">login</a>'
+  if(authIsOwner(req, res)){
+    authStatusUI = `<a href="/auth/logout">${req.session.nickname} logout</a>`
+  }
+
+  return authStatusUI;
+}
+
 router.get('/', (req, res) => {
+  
   const title = 'Welcome'
   const description = 'Hello, Node.js'
   const list = template.list(req.list)
@@ -11,7 +29,8 @@ router.get('/', (req, res) => {
       <h2>${title}</h2>${description}
       <img style="display:block;max-width:370px;margin-top:25px;" src="/images/elia.jpg" />
     `,
-    `<a href="/topic/create">create</a>`
+    `<a href="/topic/create">create</a>`,
+    authStatusUI(req, res)
   )
   res.send(html)
 });
